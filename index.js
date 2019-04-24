4d62cf2bc8ac7fe2d586f1503fc27211283e1dac
Nicolas Marini <nmarini44@gmail.com>
2019-04-24 00:07:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function printBadges (names) {
 
   return names;
 }
+
+function tailsNeverFails() {
+  let tails = 0
+
+  while (Math.random() >= .05) {
+    tails++;
+  }
+  return "You got ${tails} tails in a row!";
+}
